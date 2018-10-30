95fc473bdf403f1d45445b03e171f98edf40e6f6
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-30 19:53:45 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  messages=[]
+  for i in array
+    messages.push(badge_maker(i))
+  end
+  return messages
+end
+
+def assign_rooms(array)
+  room=1
+  assignments=[]
+  for i in array
+    assignments.push("Hello, #{i}! You'll be assigned to room #{room}!")
+    room+=1
+  end
+  return assignments
+end
+
+def printer(array)
+  for i in batch_badge_creator(array)
+    puts i
+  end
+  for i in assign_rooms(array)
+    puts i
+  end
+end
